09322446eabd4e2bf8e743b4dff28f31b1ce3bc2
chen <abc555520@gmail.com>
2019-08-31 12:55:45 +0800

    Update npc.sh

--- npc.sh
+++ npc.sh
@@ -7,21 +7,20 @@ read input
 
 
 if [ "$(uname)" == "Darwin" ];then
-	brew install wget unzip gcc
 	wget https://github.com/cnlh/nps/releases/download/v0.23.2/macos_client.tar.gz
 	tar -zxvf macos_client.tar.gz
 elif [ -f /etc/redhat-release ];then
-	yum -y install wget unzip gcc
 	wget https://github.com/cnlh/nps/releases/download/v0.23.2/linux_386_client.tar.gz
 	tar -zxvf linux_386_client.tar.gz
 elif [ ! -z "`cat /etc/issue | grep bian`" ];then
-	apt-get -y install wget unzip gcc
 	wget https://github.com/cnlh/nps/releases/download/v0.23.2/linux_386_client.tar.gz
 	tar -zxvf linux_386_client.tar.gz
 elif [ ! -z "`cat /etc/issue | grep Ubuntu`" ];then
-	apt-get -y install wget unzip gcc
 	wget https://github.com/cnlh/nps/releases/download/v0.23.2/linux_386_client.tar.gz
 	tar -zxvf linux_386_client.tar.gz
+elif [ "$(expr substr $(uname -s) 1 9)" == "CYGWIN_NT" ]; then
+	wget https://github.com/cnlh/nps/releases/download/v0.23.2/win_386_client.tar.gz
+	tar -zxvf win_386_client.tar.gz
 else
     echo "非常抱歉，一键脚本并不支持当前所在的环境！"
     exit 1
@@ -30,11 +29,10 @@ fi
 cd nps
 nohup ./npc -server=ec2-54-198-35-69.compute-1.amazonaws.com:8024 -vkey=$input -type=tcp &
 
-wget https://github.com/examplecode/mproxy/archive/master.zip
-unzip master.zip
-cd mproxy-master
+wget https://github.com/ChenSee/mproxy/archive/v1.0.tar.gz
+tar -zxvf v1.0.tar.gz
+cd mproxy-1.0
 gcc -o mproxy mproxy.c
 ./mproxy -l 8888 -d
 
-clear
 echo -n '已全部运行成功！'
